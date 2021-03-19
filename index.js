9084fef88954f6a6ffa43efe03da1b79c56a1620
francesta <huntleyfrancesta@gmail.com>
2021-03-19 00:38:23 -0400

    git@github.com:huntleyfrancesta/phase-0-the-dom-modifying-elements-lab.git

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+var el = document.getElementById('main');
+el.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+newHeader.innerHTML = "francesta is the champion"
